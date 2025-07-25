dd71f9cfe16c672f16694d0cf9594f3417c800f8
Max <maxoumixou@gmail.com>
2025-07-26 01:18:56 +0900

    Added pig assassin animations and script

---
+++ Assets/Scripts/Enemies/Enemy_PigAssassin.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy_PigAssassin : Entity_Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        idleState = new EnemyIdleState(this, stateMachine, "idle");
+        moveState = new EnemyMoveState(this, stateMachine, "move");
+        attackState = new EnemyAttackState(this, stateMachine, "attack");
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        stateMachine.Initialize(idleState);
+    }
+
+
+}
