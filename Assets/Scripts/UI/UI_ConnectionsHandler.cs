2283e86cbd53c29f5a792fd0dc01f8eba952ba51
Max <maxoumixou@gmail.com>
2025-08-15 19:06:20 +0900

    Added node connection system for skill tree connections

---
+++ Assets/Scripts/UI/UI_ConnectionsHandler.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[Serializable]
+public class UI_ConnectionDetails
+{
+    public UI_ConnectionsHandler childNode;
+    public NodeDirectionType direction;
+    [Range(0, 300f)] public float length;
+}
+
+public class UI_ConnectionsHandler : MonoBehaviour
+{
+    private RectTransform rect;
+    [SerializeField] private UI_ConnectionDetails[] connectionDetails;
+    [SerializeField] private UI_TreeConnections[] connections;
+
+    private void OnValidate()
+    {
+        if (rect == null)
+            rect = GetComponent<RectTransform>();
+
+        if (connectionDetails.Length != connections.Length)
+        {
+            Debug.Log("Amount of details should be same as amount of connections. - " + gameObject.name);
+            return;
+        }
+
+        UpdateConnection();
+        
+    }
+
+    private void UpdateConnection()
+    {
+        for (int i = 0; i < connectionDetails.Length; i++)
+        {
+            var detail = connectionDetails[i];
+            var connection = connections[i];
+
+            connection.DirectConnection(detail.direction, detail.length);
+            Vector2 targetPosition = connection.GetConnectionPoint(rect);
+            detail.childNode.SetPosition(targetPosition);
+        }
+    }
+
+    public void SetPosition(Vector2 position) => rect.anchoredPosition = position;
+}
---
+++ Assets/Scripts/UI/UI_TreeConnections.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class UI_TreeConnections : MonoBehaviour
+{
+    [SerializeField] private RectTransform rotationPoint;
+    [SerializeField] private RectTransform connectLength;
+    [SerializeField] private RectTransform childNodeConnectionPoint;
+
+    public void DirectConnection(NodeDirectionType direction, float length)
+    {
+        bool shouldBeActive = direction != NodeDirectionType.None;
+        float finalLength = shouldBeActive ? length : 0;
+        float angle = GetDirectionAngle(direction);
+
+        rotationPoint.localRotation = Quaternion.Euler(0, 0, angle);
+        connectLength.sizeDelta = new Vector2(finalLength, connectLength.sizeDelta.y);
+
+    }
+
+    public Vector2 GetConnectionPoint(RectTransform rect)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (
+            rect.parent as RectTransform,
+            childNodeConnectionPoint.position,
+            null,
+            out var localPosition
+            );
+
+        return localPosition;
+    }
+
+
+    private float GetDirectionAngle(NodeDirectionType type)
+    {
+        switch (type)
+        {
+            case NodeDirectionType.UpLeft: return 135f;
+            case NodeDirectionType.Up: return 90f;
+            case NodeDirectionType.UpRight: return 45f;
+            case NodeDirectionType.Left: return 180f;
+            case NodeDirectionType.Right: return 0f;
+            case NodeDirectionType.DownLeft: return -135f;
+            case NodeDirectionType.Down: return -90f;
+            case NodeDirectionType.DownRight: return -45f;
+            default: return 0f;
+        }
+    }
+}
+
+
+public enum NodeDirectionType
+{
+    None,
+    UpLeft,
+    Up,
+    UpRight,
+    Left,
+    Right,
+    DownLeft,
+    Down,
+    DownRight
+}
