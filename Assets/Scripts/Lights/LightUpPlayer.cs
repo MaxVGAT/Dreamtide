2e3995fa246913723025bd45c7ea1ca320c7f843
マックス <maxoumixou@gmail.com>
2025-08-28 16:19:23 +0900

    Added comments on Light script
    (cherry picked from commit 8c2128d1f227abc3fb92f7a892986ba0e64700d0)

--- Assets/Scripts/Lights/LightUpPlayer.cs
+++ Assets/Scripts/Lights/LightUpPlayer.cs
@@ -7,10 +7,10 @@ public class LightUpPlayer : MonoBehaviour
     private Light2D lampLight;
 
     [Header("Light details")]
-    [SerializeField] private Light2D playerLight;
+    [SerializeField] private Light2D playerLight; // 暗いマップ内でプレイヤーのライト参照を取得する
 
     [Header("Flickering details")]
-    [SerializeField] private float flickerInterval;
+    [SerializeField] private float flickerInterval; // 秒単位、ライトの強度を変化させてチラつきをシミュレートする間隔
     private float targetIntensity;
     private float baseIntensity = 1f;
     private float minFlicker = -0.5f;
@@ -22,7 +22,7 @@ public class LightUpPlayer : MonoBehaviour
     {
         lampLight = GetComponentInChildren<Light2D>();
 
-        playerLight.intensity = 0.5f;
+        playerLight.intensity = 0.5f; // 暗いマップ内でのプレイヤーライトの基本強度
         targetIntensity = baseIntensity;
     }
 
@@ -33,26 +33,26 @@ public class LightUpPlayer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player")) // プレイヤーがライトの下にいるときのみ影響する
             playerLight.intensity = 1.5f;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
             playerLight.intensity = 0.5f;
     }
 
     private void SetLightFlicker()
     {
-        if(timer >= flickerInterval)
+        if (timer >= flickerInterval) // ランダムなチラつき時間、古く使い込まれたランプをシミュレートする
         {
-            targetIntensity = baseIntensity + Random.Range(minFlicker, maxFlicker);
+            targetIntensity = baseIntensity + Random.Range(minFlicker, maxFlicker); // チラつき速度の範囲
             flickerInterval = Random.Range(0.05f, 0.3f);
             timer = 0;
         }
 
-        lampLight.intensity = Mathf.Lerp(lampLight.intensity, targetIntensity, Time.deltaTime * changeSpeed);
+        lampLight.intensity = Mathf.Lerp(lampLight.intensity, targetIntensity, Time.deltaTime * changeSpeed); // ライトの強度の範囲、明るさの増減
         timer += Time.deltaTime;
     }
 
