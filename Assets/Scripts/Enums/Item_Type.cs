1cc706144413b64c1b6783bb2582060ae4302a4c
Max <maxoumixou@gmail.com>
2025-08-29 02:24:53 +0900

    Revert "Changed Item_Type enum to add more armor types"
    This reverts commit 211627299968bcbadd0e6e22ac89d8d0289b2d40.

--- Assets/Scripts/Enums/Item_Type.cs
+++ Assets/Scripts/Enums/Item_Type.cs
@@ -2,23 +2,8 @@ using UnityEngine;
 
 public enum Item_Type
 {
-    // --- Material
     Material,
-
-    // --- Weapon
     Weapon,
-
-    // --- Armor
-    Helmet,
-    Shoulders,
-    Chest,
-    Pants,
-    Cape,
-    Bracers,
-    Gloves,
-    Boots,
-
-    // --- Trinkets
-    Rings,
-    Runes // Accessories (rings, amulets, books etc;)
+    Armor,
+    Trinket // Accessories (rings, amulets, books etc;)
 }
